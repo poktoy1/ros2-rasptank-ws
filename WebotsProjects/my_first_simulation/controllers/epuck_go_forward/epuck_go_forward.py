0289295e89fe8d67995fe632a8033e115bd4332f
poktoy <poktoy1@gmail.com>
2021-08-06 18:36:41 +0000

    finish tutorial 5

--- WebotsProjects/my_first_simulation/controllers/epuck_go_forward/epuck_go_forward.py
+++ WebotsProjects/my_first_simulation/controllers/epuck_go_forward/epuck_go_forward.py
@@ -2,43 +2,42 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+from controller import Robot, DistanceSensor, Motor
+
+TIME_STEP = 64
 
 # create the Robot instance.
 robot = Robot()
-MAX_SPEED = 6.28
+# create the Robot instance.
+robot = Robot()
+# initialize devices
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
+# initialize devices
+ps = []
+psNames = [
+    'ps0', 'ps1', 'ps2', 'ps3',
+    'ps4', 'ps5', 'ps6', 'ps7'
+]
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)￼ ￼￼
-
-# get the motor devices
+for i in range(8):
+    ps.append(robot.getDevice(psNames[i]))
+    ps[i].enable(TIME_STEP)
+    
 leftMotor = robot.getDevice('left wheel motor')
 rightMotor = robot.getDevice('right wheel motor')
-# set up the motor speeds at 10% of the MAX_SPEED.
-
 leftMotor.setPosition(float('inf'))
 rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0.0)
+rightMotor.setVelocity(0.0)
 
-leftMotor.setVelocity(0.5 * MAX_SPEED)
-rightMotor.setVelocity(0.5 * MAX_SPEED)
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
-
-# Enter here exit cleanup code.
+# feedback loop: step simulation until receiving an exit event
+while robot.step(TIME_STEP) != -1:
+    # read sensors outputs
+    # process behavior
+    # write actuators inputs
+    # read sensors outputs
+    psValues = []
+    for i in range(8):
+        psValues.append(ps[i].getValue()
+    
