03668fc6891f08e98ab07ee0d809d7c2e1291c1d
poktoy <poktoy1@gmail.com>
2021-08-12 18:03:39 +0000

    added servo arm subscriber /tf

--- src/tank/tank/subscriber_arm.py
+++ src/tank/tank/subscriber_arm.py
@@ -1,7 +1,16 @@
 
-from adafruit_servokit import ServoKit
+try:
+    from adafruit_servokit import ServoKit
+except Exception:
+    pass
 import rclpy
 from rclpy.node import Node
+from tf2_msgs.msg import TFMessage
+from geometry_msgs.msg import TransformStamped
+from geometry_msgs.msg import Quaternion
+import tf_transformations
+from math import degrees as Degrees
+
 
 JOINT_GRIP = {
     'max': 90,
@@ -16,17 +25,17 @@ JOINT_WRIST = {
     'joint': 14
 }
 
-JOINT_ARM = {
+JOINT_ELBOW = {
     'max': 140,
     'min': 0,
     'angle': 0,
     'joint': 13
 }
 
-JOINT_ELBOW = {
-    'max': 160,
-    'min': 10,
-    'angle': 0,
+JOINT_SHOULDER = {
+    'max': 140,
+    'min': 20,
+    'angle': 90,
     'joint': 12
 }
 
@@ -34,15 +43,21 @@ JOINT_ELBOW = {
 class ServoArm():
 
     def __init__(self):
-        self._kit = ServoKit(channels=16)
-        self._kit.servo[JOINT_GRIP['joint']].angle = JOINT_GRIP['max']
-        self._kit.servo[JOINT_WRIST['joint']].angle = JOINT_WRIST['min']
-        self._kit.servo[JOINT_ARM['joint']].angle = JOINT_ARM['min']
-        self._kit.servo[JOINT_ELBOW['joint']].angle = JOINT_ELBOW['min']
+        try:
+            self._kit = ServoKit(channels=16)
+            self._kit.servo[JOINT_GRIP['joint']].angle = JOINT_GRIP['max']/2
+            self._kit.servo[JOINT_WRIST['joint']].angle = JOINT_WRIST['max']/2
+            self._kit.servo[JOINT_ELBOW['joint']].angle = JOINT_ELBOW['max']/2
+            self._kit.servo[JOINT_SHOULDER['joint']].angle = JOINT_SHOULDER['max']/2
+        except Exception:
+            pass
 
     def moveJoint(self, joint: int, angle: int, max: int, min: int):
-        if(min < angle < max):
-            self._kit.servo[joint].angle = angle
+        try:
+            if(min < angle < max):
+                self._kit.servo[joint].angle = angle
+        except Exception:
+            pass
 
 
 class SubscriberArm(Node):
@@ -52,22 +67,68 @@ class SubscriberArm(Node):
         self.servo_arm_ = servoArm
         self.i = 0
         # test
-        timer_period = 5  # seconds
-        self.timer = self.create_timer(timer_period, self.timer_callback)
+        # timer_period = 0.7  # seconds
+        # self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.dec_flag_ = False
+        self.still_moving = False
+        self.subscription = self.create_subscription(
+            TFMessage,
+            '/tf',
+            self.listener_callback,
+            10)
+        self.subscription
+        print('initialized')
+
+    def listener_callback(self, msg: TFMessage):
+        transforms = msg.transforms
+        for transformStamped in transforms:
+
+            deg = self.get_deg(transformStamped)
+            if(transformStamped.child_frame_id == 'single_rrbot_link2'):
+                JOINT_SHOULDER['angle'] = deg
+                self.servo_arm_.moveJoint(**JOINT_SHOULDER)
+            elif (transformStamped.child_frame_id == 'single_rrbot_link3'):
+                JOINT_ELBOW['angle'] = deg
+                self.servo_arm_.moveJoint(**JOINT_ELBOW)
+        
+        # self.servo_arm_.moveJoint(**JOINT_ELBOW)
+
+    def get_deg(self, transformStamped: TransformStamped):
+        quaternion: Quaternion = transformStamped.transform.rotation
+        print(f'{transformStamped.child_frame_id}:{quaternion}')
+        r,p,y = tf_transformations.euler_from_quaternion(
+            [quaternion.w, quaternion.x, quaternion.y, quaternion.z])
+        # print(f'euler:{r},{p},{y}')
+        deg = 90 - Degrees(p)
+        print(f'deg:{deg}')
+        return deg
+
 
     def timer_callback(self):
-        self.i += 20
-        if(self.i > 180):
-            self.i = 0
+        if(self.still_moving == False):
+            self.still_moving = True
+            if(self.i > 180):
+                self.dec_flag_ = True
+            elif(self.i < 0):
+                self.i = 0
+                self.dec_flag_ = False
+
+            if(self.dec_flag_):
+                self.i -= 1
+            else:
+                self.i += 1
+            self.still_moving = False
+
         JOINT_GRIP['angle'] = self.i
         JOINT_WRIST['angle'] = self.i
-        JOINT_ARM['angle'] = self.i
         JOINT_ELBOW['angle'] = self.i
+        JOINT_SHOULDER['angle'] = self.i
         self.servo_arm_.moveJoint(**JOINT_GRIP)
         self.servo_arm_.moveJoint(**JOINT_WRIST)
-        self.servo_arm_.moveJoint(**JOINT_ARM)
         self.servo_arm_.moveJoint(**JOINT_ELBOW)
+        self.servo_arm_.moveJoint(**JOINT_SHOULDER)
         print(f'i:${self.i}')
+
 
 
 def main(args=None):
